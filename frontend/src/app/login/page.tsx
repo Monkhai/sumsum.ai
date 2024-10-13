30f784ae32ea670cc37b3753e254c5c7cef1b0ff
Yohai Wiener <yohaiwiener@gmail.com>
2024-10-13 14:32:05 +0300

    add animations to login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,16 +4,27 @@ import React from "react";
 import GoogleButton from "react-google-button";
 import { auth } from "~/firebase/firebase";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function index() {
   const authProvider = new GoogleAuthProvider();
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
+    <div className="relative flex h-screen w-screen flex-col items-center justify-center gap-8">
       <div className="relative flex flex-col items-center gap-2">
-        <motion.h1
+        <motion.img
           initial={{ opacity: 0, bottom: -50, scale: 0.5 }}
           animate={{ opacity: 1, bottom: 0, scale: 1 }}
           transition={{ duration: 1 }}
+          src="/logo.webp"
+          width={50}
+          height={50}
+          alt="logo"
+          className="relative rounded-full object-contain"
+        />
+        <motion.h1
+          initial={{ opacity: 0, bottom: -50, scale: 0.5 }}
+          animate={{ opacity: 1, bottom: 0, scale: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
           className="relative text-2xl font-medium"
         >
           Welcome to SumSum.ai
@@ -22,21 +33,25 @@ export default function index() {
           initial={{ opacity: 0, bottom: -50, scale: 0.5 }}
           animate={{ opacity: 1, bottom: 0, scale: 1 }}
           className="relative text-lg font-medium"
-          transition={{ delay: 0.5, duration: 1 }}
+          transition={{ delay: 1, duration: 1 }}
         >
           Simplifying your workflow with AI-powered markdown summaries
         </motion.h2>
       </div>
-      <GoogleButton
-        type="dark"
-        style={{
-          borderRadius: "10px",
-          overflow: "hidden",
-        }}
-        onClick={() => signInWithPopup(auth, authProvider)}
+      <motion.div
+        initial={{ opacity: 0, bottom: -50, scale: 0.5 }}
+        animate={{ opacity: 1, bottom: 0, scale: 1 }}
+        transition={{ delay: 1.5, duration: 1 }}
+        className="relative"
       >
-        login
-      </GoogleButton>
+        <GoogleButton
+          type="dark"
+          style={{ borderRadius: "10px", overflow: "hidden" }}
+          onClick={() => signInWithPopup(auth, authProvider)}
+        >
+          login
+        </GoogleButton>
+      </motion.div>
     </div>
   );
 }
